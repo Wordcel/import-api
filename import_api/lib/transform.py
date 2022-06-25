95078388d1d05d2f103fda2f3d10369a056dc323
abishekk92 <bhat.abishek@gmail.com>
2022-06-25 15:12:45 +0530

    Initial Commit

---
+++ import_api/lib/transform.py
@@ -0,0 +1,74 @@
+class BlockTransform(object):
+
+    def __init__(self, html):
+        self.html = html
+
+    def embed_tweet(self, html):
+        links = html.find_all('a')
+        if len(links):
+            tweet = links[-1].attrs["href"]
+            content = {"service": "twitter",
+                       "source": tweet,
+                       "embed": f"https://twitframe.com/show?url={tweet}",
+                       "width": 600,
+                       "height": 300,
+                       "caption": ""
+                       }
+            return {"type": "embed", "data": content}
+
+    def paragraph(self, node):
+        content = "".join([str(c) for c in node.contents])
+        return {"type": "paragraph", "data": {"text": content}}
+
+    def header(self, node, level):
+        return {"type": "header", "data": {
+            "level": level, "text": node.text}}
+
+    def html_list(self, node, list_type):
+        block = {"type": "list", "data": {"style": "", "items": []}}
+        if list_type == "ol":
+            block["data"]["style"] = "ordered"
+        elif list_type == "ul":
+            block["data"]["style"] = "unordered"
+        items = node.select("li")
+        for item in items:
+            content = "".join([str(c) for c in item.contents])
+            block["data"]["items"].append(content)
+        return block
+
+    def convert_prime(self, blocks, html=None):
+        if not html:
+            html = self.html
+        for i in html.children:
+            name = i.name
+            if not name:
+                continue
+            if name in ['html', 'body', 'div', 'a']:
+                blocks += self.convert_prime(blocks, i)
+            elif name == 'p':
+                block = self.paragraph(i)
+                blocks.append(block)
+            elif len(name) == 2 and name.startswith("h"):
+                level = name[-1]
+                block = self.header(i, level)
+                blocks.append(block)
+            elif name in ['ol', 'ul']:
+                block = self.html_list(i, name)
+                blocks.append(block)
+            elif name == 'blockquote':
+                content = i.encode_contents()
+                if b"twitter" in content:
+                    block = self.embed_tweet(i)
+                else:
+                    block = {"type": "blockquote",
+                             "data": {"text": str(content)}}
+                blocks.append(block)
+            elif name == "img":
+                content = {"url": i.attrs["src"],
+                           "caption": i.attrs["alt"]}
+                block = {"type": "image", "data": content}
+                blocks.append(block)
+            else:
+                print(name)
+                print(i)
+        return blocks
---
+++ import_api/main.py
@@ -0,0 +1,39 @@
+from newspaper import Article, Source
+from markdownify import markdownify as md
+
+from typing import Union
+
+from fastapi import FastAPI
+from typing import Literal
+from pydantic import HttpUrl
+from bs4 import BeautifulSoup
+
+from .lib.transform import BlockTransform
+
+app = FastAPI(title="Article Import API", version="0.1.0")
+
+
+@app.get("/")
+async def index():
+    return {"msg": "gm"}
+
+
+@app.get("/import")
+def process_url(url: HttpUrl, doc_type: Literal['blocks', 'markdown'] = 'blocks'):
+    article = Article(url, keep_article_html=True)
+    article.download()
+    article.parse()
+    data = {
+        "title": article.title,
+        "authors": article.authors,
+        "header_image": article.top_image,
+        "images": article.images,
+        "videos": article.movies,
+    }
+    if doc_type == "blocks":
+        html_doc = BeautifulSoup(article.article_html)
+        transformer = BlockTransform(html_doc)
+        data["blocks"] = transformer.convert_prime(blocks=[])
+    elif doc_type == "markdown":
+        data["markdown"] = md(article.article_html, newline_styles='backslash')
+    return data
